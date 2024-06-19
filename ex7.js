f5788c1426fbfdd7fca1799b0472f572ba23b02d
omiras <oscar.miras@gmail.com>
2024-06-19 21:42:45 +0200

    ex7 done

--- ex7.js
+++ ex7.js
@@ -6,6 +6,10 @@ function calcularEdad(persona) {
     let newPerson = {};
 
     // Modificar a partir de aqui
+    newPerson = {
+        ...persona,
+        edad: currentYear - persona.nacimiento
+    }
 
     // NO modificar a partir de aquí
     return newPerson;
@@ -15,9 +19,9 @@ function calcularEdad(persona) {
 console.log(calcularEdad({
     nombre: 'Marcos',
     nacimiento: '1999'
-})); // { nombre: 'Marcos', nacimiento: '1999', edad: 24}
+})); // { nombre: 'Marcos', nacimiento: '1999', edad: el valor debería ser el año actual en el que estamos menos la edad de nacimiento de Marcos}
 
 console.log(calcularEdad({
     nombre: 'Maria',
     nacimiento: '2003'
-})); // { nombre: 'Maria', nacimiento: '2003', edad: 20}
+})); // { nombre: 'Maria', nacimiento: '2003', edad: el valor debería ser el año actual en el que estamos menos la edad de nacimiento de Maria}
